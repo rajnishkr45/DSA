91f01d3f0f32be29d8d82066658d0b5b7724e2ed
Rajnish Kumar <rajnishroushan2020@gmail.com>
2025-08-10 20:18:06 +0530

    1st code uploaded

---
+++ basics/find_prime.cpp
@@ -0,0 +1,32 @@
+// Find prime number till n
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+
+    int n;
+    cin >> n;
+
+    for (int i = 2; i <= n; i++)
+    {
+        bool isPrime = true;
+
+        for (int j = 2; j <= sqrt(i); j++)
+        {
+            if (i % j == 0)
+            {
+                isPrime = false;
+                break;
+            }
+        }
+
+        if (isPrime)
+        {
+            cout << i << " is prime " << endl;
+        }
+    }
+
+    return 0;
+}
