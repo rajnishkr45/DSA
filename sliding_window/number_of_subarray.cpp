09831538b7fcd6c9d01a8f2322beeb4cd7034105
Rajnish Kumar <rajnishroushan2020@gmail.com>
2025-08-26 20:18:47 +0530

    Introductory part done

---
+++ sliding_window/number_of_subarray.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    vector<int> arr = {1, 5, 2, 6, 3, 8, 6, 4, 8, 4};
+
+    // To find the number of subarray first of all find
+    // x = Number of subarray whose sum is <= k
+    // y = Number of subarray whose sum is <= k-1
+
+    // Total number = x - y
+    return 0;
+}
---
+++ sliding_window/shortest_subarray.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    vector<int> arr = {1, 5, 2, 6, 3, 8, 6, 4, 8, 4};
+
+    // Similar to longest one but in this case just keep shrinking the window
+    return 0;
+}
