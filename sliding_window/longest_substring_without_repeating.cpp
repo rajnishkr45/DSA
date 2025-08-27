01c5d66943bd355e53aa68dbc105724ddb562491
Rajnish Kumar <rajnishroushan2020@gmail.com>
2025-08-27 23:57:43 +0530

    new code

---
+++ sliding_window/longest_substring_without_repeating.cpp
@@ -0,0 +1,13 @@
+// Find the longest substring without repeating the character
+
+// Bruteforce method
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string str = "asgrtabjdtar";
+    
+
+    return 0;
+}
